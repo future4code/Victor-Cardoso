274f32c4bbae028f83c226b2045f0f1ce92e02e1
cptvictor95 <cpt.victor@hotmail.com>
2020-12-11 13:06:23 -0300

    exercício 1 de interpretação feito

---
+++ lista-de-exercicios/index.js
@@ -0,0 +1,7 @@
+// Exercícios de interpretação de código
+// Exercício 1
+/*
+É declarada uma função que guarda um número digitado pelo usuário para retornar a conversão (multiplicação do argumento x a cotação) em dólar para reais, o valorEmDolar é um argumento da função.
+Fora e depois da função tem uma variável que guarda o meuDinheiro, e recebe a função de conversão com o parâmetro valorEmDolar de 100.
+Por último imprime o valor da variável meuDinheiro, que é 100 multiplicado pela cotação em dólares
+*/
