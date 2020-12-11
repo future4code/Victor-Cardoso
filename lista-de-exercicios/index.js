21456ebc54a67d82d4b7d530fa709e279fcb8dd7
cptvictor95 <cpt.victor@hotmail.com>
2020-12-11 13:06:53 -0300

    exercício 2 de interpretação feito

--- lista-de-exercicios/index.js
+++ lista-de-exercicios/index.js
@@ -5,3 +5,10 @@
 Fora e depois da função tem uma variável que guarda o meuDinheiro, e recebe a função de conversão com o parâmetro valorEmDolar de 100.
 Por último imprime o valor da variável meuDinheiro, que é 100 multiplicado pela cotação em dólares
 */
+
+// Exercício 2
+/*
+É declarada uma função com 2 parâmetros: tipoDeInvestimento e valor, com uma variável declarada para guardar o valorAposInvestimento de acordo com tipoDeInvestimento, a validação do tipoDeInvestimento é feita com switch case para cada caso (poupança, renda fixa, cdb e ações), em cada caso o valorAposInvestimento recebe o argumento valor multiplicado pela porcentagem de cada caso, o default do switch case é um alerta avisando que o tipo de investimento não foi identificado dentro dos casos. A função retorna a variável valorAposInvestimento.
+Fora e depois da função duas variáveis para montante são declaradas, novoMontante que recebe a função investeDinheiro com o tipoDeInvestimento Ações e 150 de valor, e segundoMontante que recebe a função investeDinheiro com o tipoDeInvestimento Tesouro Direto e 200 de valor.
+Por último imprime no console novoMontante e segundoMontante.
+*/
