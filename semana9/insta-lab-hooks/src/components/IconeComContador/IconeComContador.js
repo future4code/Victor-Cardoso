f11b732a28aa94af24c48efc511aba90b6e3d200
cptvictor95 <cpt.victor@hotmail.com>
2021-01-25 17:01:42 -0300

    projeto de exercícios completo

---
+++ semana9/insta-lab-hooks/src/components/IconeComContador/IconeComContador.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const IconeContainer = styled.div`
+  display: flex;
+
+  img {
+    margin-right: 5px;
+    cursor: pointer;
+  }
+`;
+
+function IconeComContador(props) {
+  return (
+    <IconeContainer>
+      <img alt={"Icone"} src={props.icone} onClick={props.onClickIcone} />
+      <p>{props.valorContador}</p>
+    </IconeContainer>
+  );
+}
+
+export default IconeComContador;
