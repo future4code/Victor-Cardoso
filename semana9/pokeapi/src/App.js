a72eb5f542ecef5ed25650ca135317b376a14b0c
cptvictor95 <cpt.victor@hotmail.com>
2021-01-26 14:37:54 -0300

    select and map pokemons working

--- semana9/pokeapi/src/App.js
+++ semana9/pokeapi/src/App.js
@@ -1,24 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import PokeCard from "./components/pokeCard/PokeCard";
 import { Main, Heading2 } from "./globalStyle";
 
 const App = () => {
   const title = "pokeList";
   const [pokeList, setPokeList] = useState([]);
-  const [pokemon, setPokemon] = useState(null);
+  const [pokeName, setPokeName] = useState("");
 
   // TO DO: onChangePokemon
+  const onChangePokeName = (event) => {
+    setPokeName(event.target.value);
+  };
 
   // TO DO: getPokeList
+  const getPokeList = () => {
+    axios
+      .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((res) => {
+        // console.log(res.data.results);
+        setPokeList(res.data.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   // TO DO: useEffect with getPokeList
+  useEffect(() => {
+    getPokeList();
+    return () => {
+      // cleanup;
+    };
+  }, []);
 
   return (
     // TO DO: select with pokemon names
     // TO DO: render pokeList map
     <Main>
       <Heading2>{title}</Heading2>
-      <PokeCard />
+      <select name="" id="" onChange={onChangePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map((pokemon) => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+      <PokeCard pokemon={pokeName} />
     </Main>
   );
 };
--- semana9/pokeapi/src/components/pokeCard/PokeCard.js
+++ semana9/pokeapi/src/components/pokeCard/PokeCard.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Section, Text } from "./styles.js";
 
 const PokeCard = (props) => {
+  // TO DO: useState pokemon object
+
+  // TO DO: getPokemon
+
+  // TO DO: useEffect to get PokemonByName
+
   return (
     <Section>
       <Text>Name: {props.name}</Text>
       <Text>Img: {props.img}</Text>
       <Text>Weight: {props.weight}</Text>
+      <Text>Type: {props.type}</Text>
     </Section>
   );
 };
